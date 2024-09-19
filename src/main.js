3b45e632bbc7ad3ccf09eabade0b69241ae22dd1
Riary2024 <alejoyepes84@outlook.es>
2024-09-18 21:02:02 -0500

    Actualización de index.html y src/main.js Lazy loading

--- src/main.js
+++ src/main.js
@@ -10,21 +10,35 @@ const api = axios.create({
 });
 
 // Utils 
-function createMovies(movies, container) {
+const lazyLoader = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+       const url = entry.target.getAttribute('data-img')
+       entry.target.setAttribute('src',url)
+      }
+    });
+});
+
+
+
+function createMovies(movies, container, lazyLoad = false) {
     container.innerHTML = '';
 
     movies.forEach(movie => {
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
-       movieContainer.addEventListener('click', () => {
+        movieContainer.addEventListener('click', () => {
            location.hash = '#movie=' + movie.id;
      });
 
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
         movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+        movieImg.setAttribute(lazyLoad ? 'data-img' : 'src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
 
+        if (lazyLoad) {
+        lazyLoader.observe(movieImg);
+        }
         movieContainer.appendChild(movieImg);
         container.appendChild(movieContainer);
     });
@@ -57,7 +71,7 @@ async function getTrendingMoviesPreview() {
     const movies = data.results;
     console.log(movies);
 
-    createMovies(movies, trendingMoviesPreviewList); 
+    createMovies(movies, trendingMoviesPreviewList, true); 
 }
 
 // Categories con axios: 
